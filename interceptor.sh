5fc34525cbdb453fce7b79298564ea39ddc297ed
FrancescoDiSalesGithub <f.disales@hotmail.com>
2024-09-30 22:39:15 +0200

    adding interceptor

---
+++ interceptor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+while [ True ]
+do
+
+	
+	echo "insert http request: "
+	
+	input=""
+	while IFS= read -r line; do
+    		input+="$line"$'\n'
+	done
+
+	echo "insert host to forward request:"
+	host=read host
+	echo "insert port"
+	port=read port
+
+
+	echo $input | nc $host $port
+done
