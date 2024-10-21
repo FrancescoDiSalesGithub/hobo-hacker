1e18800f9e0b5a80bddde301cb014bdf940605ec
FrancescoDiSalesGithub <f.disales@hotmail.com>
2024-10-21 18:52:23 +0200

    adding sha crackers

---
+++ sha1cracker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+for  item in $(cat $2)
+do
+
+    hash=$(echo $item | sha1sum | awk '{print $1}')
+
+    if [ "$hash" == "$1" ] 
+    then
+
+        echo "hash cracked => plaintext value: $item"
+        break
+
+    fi
+
+done
