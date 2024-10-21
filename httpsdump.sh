683b9b63b72c71ed2b407ecc1e44e2445d4cdf6a
FrancescoDiSalesGithub <f.disales@hotmail.com>
2024-10-21 19:51:29 +0200

    adding httpsdump script

---
+++ httpsdump.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SSLKEYLOGFILE=~/sslkeys.log
+user=$(whoami)
+interface=$1
+
+
+if [ "$user" == "root" ]
+then
+    echo "listening on every interface"
+    sudo tcpdump -i $interface -w https_traffic.pcap port 443
+else
+    echo "please run this script as root"
