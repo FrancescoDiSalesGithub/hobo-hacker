fe08fc0e434be5365301f432c42c98cd00937a7f
FrancescoDiSalesGithub <f.disales@hotmail.com>
2024-10-21 20:03:20 +0200

    https adding personal ssl log file

--- httpsdump.sh
+++ httpsdump.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-export SSLKEYLOGFILE=~/sslkeys.log
+export SSLKEYLOGFILE=$2
+
+if [ "$SSLKEYLOGFILE" == "" ]
+then
+    echo "please insert path for ssl log file"
+else
+
 user=$(whoami)
 interface=$1
 
 
-if [ "$user" == "root" ]
-then
-    echo "listening on every interface"
-    sudo tcpdump -i $interface -w https_traffic.pcap port 443
-else
-    echo "please run this script as root"
+    if [ "$user" == "root" ]
+    then
+        echo "listening on every interface"
+        sudo tcpdump -i $interface -w https_traffic.pcap port 443
+    else
+        echo "please run this script as root"
+    fi
+
 fi
