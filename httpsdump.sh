c24e45a8ddd88e90a0401242257c96c0b03ca29c
FrancescoDiSalesGithub <f.disales@hotmail.com>
2024-10-24 22:29:48 +0200

    fixing https dump

--- httpsdump.sh
+++ httpsdump.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-export SSLKEYLOGFILE=$2
+export SSLKEYLOGFILE=$2/sslkeys.log
 
 if [ "$SSLKEYLOGFILE" == "" ]
 then
     echo "please insert path for ssl log file"
 else
 
-user=$(whoami)
-interface=$1
+    interface=$1
+    program_exists=$(ls /bin | grep chromium)
 
 
-    if [ "$user" == "root" ]
+    if [ "$program_exists" != ""]
     then
-        echo "listening on every interface"
-        sudo tcpdump -i $interface -w https_traffic.pcap port 443
+        echo "listening on interface: $interface"
+
+        chromium --no-sandbox &
+        sleep 5
+        sudo tcpdump -i $interface -w https_traffic.pcap port 443 
+        
     else
-        echo "please run this script as root"
+
     fi
 
+      
+    
+    
 fi
