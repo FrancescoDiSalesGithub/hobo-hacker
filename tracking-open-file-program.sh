ccb0da996aa873c1a1f22c77793c3bd3216fdf6a
FrancescoDiSalesGithub <f.disales@hotmail.com>
2024-11-01 22:50:36 +0100

    adding saving file to tracking open file

--- tracking-open-file-program.sh
+++ tracking-open-file-program.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-strace -e trace=open,close,read,write $1
+log=$2
+
+if [ "$log" != "" ]
+then
+    strace -e trace=open,close,read,write $1
+else
+    strace -e trace=open,close,read,write $1 > $log
+fi
